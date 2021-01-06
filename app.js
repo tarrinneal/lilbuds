dd2abdbaf4170040f95b183ad99a0c86f7c369b6
Jack <jackpronske@yahoo.com>
2021-01-06 09:52:41 -0800

    Started implementing stuff from twiddler to the app.js file, nothing new yet, just getting base things set

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+//A lot of these files are just borrowed from twiddler, I figured we can just adjust things as we go.
+
 $(document).ready(function() {
 
+
+  //select already existing elements
+  var $app = $('#app');
+  $app.html('');
+
+  //create new HTML elements
+  var $header = $('<div class="header"></div>');
+  var $title = $('<h1>Little Buds</h1>');
+
+  //event handler functions
+
+  //set event listeners (providing appropriate handlers as input)
+
+  //append new HTML elements to the DOM
+
+  $header.appendTo($app);
+  $title.appendTo($header);
+
 });
+
