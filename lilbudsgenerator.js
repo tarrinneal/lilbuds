52b058d64f934639ee6412a401331aefb9f65630
tarrinneal <tarrinneal@gmail.com>
2021-01-05 23:34:05 -0800

    added 'gandalf' and 'sam' for practice fights, assets added as well

---
+++ lilbudsgenerator.js
@@ -0,0 +1,21 @@
+let gandalf = {
+  type: 'rock',
+  maxHp: 20,
+  currentHp: 20,
+  baseAtk: 10,
+  specialAtk: 17,
+  moves: ['glare', 'monch', 'tail smack', 'bask'],
+  description: 'Gandalf is a magical bearded dragon who\'s small size is made up for with his powerful monch',
+  pic: assets/ganbooty.png,
+}
+
+let sam = {
+  type: 'ground',
+  maxHp: 25,
+  currentHp: 25,
+  baseAtk: 12,
+  specialAtk: 5,
+  moves: ['chomp', 'scratch', 'sleep', 'slammy wammy'],
+  description: 'Sam is big ol cuddly bear-dog with a heart as big as his head',
+  pic: assets/sam.png,
+}
