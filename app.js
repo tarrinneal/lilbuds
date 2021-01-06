c60590ed45f94e9982da6ee6fc28bcca1b9cfc18
Jack <jackpronske@yahoo.com>
2021-01-06 11:37:42 -0800

    messed around with some format of getting into a battle and clearing out the current html this is just a test, really. I'm not married to this format, but it's a good way to see how to switch from screen to screen

--- app.js
+++ app.js
@@ -7,12 +7,43 @@ $(document).ready(function() {
   var $app = $('#app');
   $app.html('');
 
+
+
+
+
+
   //create new HTML elements
-  var $header = $('<div class="header"></div>');
-  var $title = $('<h1>Little Buds!</h1>');
+  //the header Little Buds is in the html file, because it will be the header for all the different scenes
+
+  var $homeScreenHousing = $('<section class="home-screen-housing"></section>');
+  //let's battle! button
+  var $battleStartButton = $('<button class="battle-start-button">Let\'s Battle!</button>');
+
+
+
+
+  //choose your bud
+  //this isn't final, just a test code to see how a battle can work as if it were a function that we called
+  //ex ===>  battle(myBud, enemyBud)
+
   var $battleScreen = $('<section class="battle-screen"></section>');
 
+
+
+
+
+
+
+
   //event handler functions
+
+
+  var handleStartButtonClick = function(event) {
+    console.log(event.target);
+  }
+
+
+
   var generateMoveButtons = function(bud) {
     var moveList = bud.moves;
     var $moveListBox = $('<div class="move-list-box"></div>');
@@ -23,14 +54,33 @@ $(document).ready(function() {
     $moveListBox.appendTo($battleScreen);
   }
 
+
+
+
+
+
+
+
+
   //set event listeners (providing appropriate handlers as input)
+
+  $battleStartButton.on("click", handleStartButtonClick)
+
+
+
+
+
+
+
+
 
   //append new HTML elements to the DOM
 
-  $header.appendTo($app);
-  $title.appendTo($header);
-  $battleScreen.appendTo($app);
-  generateMoveButtons(sam);
+  $homeScreenHousing.appendTo($app);
+  $battleStartButton.appendTo($homeScreenHousing);
+
+  // $battleScreen.appendTo($app);
+  // generateMoveButtons(sam);
 
 
 
