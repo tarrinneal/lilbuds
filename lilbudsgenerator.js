3f4b6cdf62fd0f26a897f67f94e8edaba9d07b4f
Jack <jackpronske@yahoo.com>
2021-01-06 09:34:47 -0800

    added lib folder and added the jQuery library to it, as well as attached the script to the HTML file fixed the pics in the animal objects (made them strings, so that the code works for now)

--- lilbudsgenerator.js
+++ lilbudsgenerator.js
@@ -6,7 +6,7 @@ let gandalf = {
   specialAtk: 17,
   moves: ['glare', 'monch', 'tail smack', 'bask'],
   description: 'Gandalf is a magical bearded dragon who\'s small size is made up for with his powerful monch',
-  pic: assets/ganbooty.png,
+  pic: 'assets/ganbooty.png'
 }
 
 let sam = {
@@ -17,5 +17,6 @@ let sam = {
   specialAtk: 5,
   moves: ['chomp', 'scratch', 'sleep', 'slammy wammy'],
   description: 'Sam is big ol cuddly bear-dog with a heart as big as his head',
-  pic: assets/sam.png,
+  pic: 'assets/sam.png'
 }
+
