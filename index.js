beb629d3fe486da4fc7d668b3131dc063850fb35
tarrinneal <tarrinneal@gmail.com>
2021-01-07 17:22:09 -0800

    fixed server bug?

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const assetDir = path.resolve('public')
 
 app.use(express.static('public'));
