fe6a9fbadc497f03ee34f0fad2d15629697864d0
tarrinneal <tarrinneal@gmail.com>
2021-01-07 17:19:07 -0800

    Finished server

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+const port = 3000;
+const assetDir = path.resolve('public')
 
-app.use(express.static())
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(assetDir + '/lilbuds.html');
+})
 
 app.listen(port, () => {
-  console.log('server listening')
+  console.log('server listening port: ' + port)
 });
